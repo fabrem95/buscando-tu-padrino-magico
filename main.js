4b969ac12927a6e7be43a857fde57dbe0799bddf
Fabrem <fjbremermann@gmail.com>
2022-11-21 21:23:31 -0300

    Endpoint para solicitar los FormEnums

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
+const fs = require("fs");
+
 const express = require("express");
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.get("/getFormEnums", function (req, res) {
+	try {
+		const FormEnums = JSON.parse(
+			fs.readFileSync(__dirname + "/data/FormEnums.json")
+		);
+
+		res.send(FormEnums);
+
+		res.end();
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 app.get("*", function (req, res) {
 	res.sendFile(__dirname + "/public/index.html");
